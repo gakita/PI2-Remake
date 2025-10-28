f4bd908c10e984226d8df6c474898be3cc59ba74
gakita <raulgru5@gmail.com>
2025-10-27 21:47:24 -0300

    feat: funções de carregar rotas criadas, ambas em desenvolvimento

---
+++ frontend/assets/js/loadTrips.js
@@ -0,0 +1,32 @@
+import { createCard } from "./createCard.js"
+
+const api = axios.create({
+    baseURL: "http://localhost:3001/api",
+    timeout: 1000,
+    Headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+const gridCards = document.getElementById("grid-cards")
+
+document.addEventListener("DOMContentLoaded", () =>{
+    loadTrips()
+})
+
+
+async function loadTrips(){
+    try{
+        
+        const response = await axios.get("http://localhost:3001/api/cities")
+        const trips = response.data
+        trips.forEach(cities => {
+            let generatedRandomPrice = (Math.floor(Math.random() * 1000))
+            let card = createCard(cities.name, generatedRandomPrice, cities.imagePath)
+            gridCards.appendChild(card.cloneNode(true))
+        })
+        console.log(trips)
+    }catch(error){
+        console.log(error)
+    }
+}
