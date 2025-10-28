da615b440c0281da52588674e88854c587b6ce48
gakita <raulgru5@gmail.com>
2025-10-27 22:35:37 -0300

    fix: arrumada o envio de imagem para o frontend

--- backend/app.ts
+++ backend/app.ts
@@ -2,10 +2,13 @@ import express from "express"
 import routes from "./src/routes/routes"
 // @ts-ignore
 import cors from "cors"
+import path from "path"
 
 const app = express()
 app.use(cors())
 
+// Servir arquivos estáticos da pasta uploads
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use(express.json())
 app.use("/api", routes)
