a1312ba0a4fa2173dfaf7525903e8aaaebe0a6d8
gakita <raulgru5@gmail.com>
2025-10-30 18:10:53 -0300

    feat: adicionada a possibilidade de pesquisa de cidades por nome

--- backend/src/features/getCitiesByName/getCitiesByName.controller.ts
+++ backend/src/features/getCitiesByName/getCitiesByName.controller.ts
@@ -1,14 +1,13 @@
 import { validateCityNameSchema } from "./getCitiesByName.schema";
 import { getCitiesByNameService } from "./getCitiesByName.service";
 import { Request, Response, NextFunction } from "express";
+import { capitalizeFirstLetter } from "../../helper/capitalizeFirstLetter";
 
 export async function getCitiesByNameController(req:Request, res:Response, next:NextFunction){
     try{
         const {name} = validateCityNameSchema.parse(req.params)
-        const cities = await getCitiesByNameService(name)
-        if (!cities || cities === null || cities == undefined) {
-            return res.status(404).json({message:"Cities not found"})
-        }
+        const nameCapitalize = capitalizeFirstLetter(name)
+        const cities = await getCitiesByNameService(nameCapitalize)
         return res.status(200).json(cities)
     }catch(error){
         next(error)
