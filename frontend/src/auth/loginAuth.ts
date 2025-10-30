b9425f65fe93d02e9bc321153298f566c5dad6b4
gakita <raulgru5@gmail.com>
2025-10-30 01:19:36 -0300

    refactor: removidos itens para atualização de código(repaginação geral do frontend)

--- frontend/src/auth/loginAuth.ts
+++
@@ -1,6 +0,0 @@
-import api from "../server"
-
-export const loginAuth = async (email: string, password: string) => {
-    const response = await api.post("/login", {email, password})
-    return response.data.name
-}
