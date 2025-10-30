ee7a4c626286fad05db15617ad51c431ca40db36
gakita <raulgru5@gmail.com>
2025-10-30 17:48:21 -0300

    feat: adicionada a criptografia de senha ao registrar uma conta, é também validada de acordo com as validações

--- backend/src/features/registerUser/registerUser.service.ts
+++ backend/src/features/registerUser/registerUser.service.ts
@@ -1,11 +1,19 @@
 import { createUserRepo } from "./registerUser.repo"
+import { userSchema } from "./UserValidation.schema"
+import {hashPassword } from "../../utils/hash"
 
-export const registerUserService = async (body:any) =>{
-    return createUserRepo.createUser({
-        name: body.name,
-        email: body.email,
-        password: body.password,
-        avatarPath: body.avatarPath,
-        isAdmin: body.isAdmin === "true"
-    })
+export const registerUserService = async ({name,email,password,avatarPath,isAdmin}) =>{
+    try{
+        console.log("registering user...")
+        try{
+            const hash = await hashPassword(password)
+            const user = ({name, email,password:hash,avatarPath,isAdmin:isAdmin === "true"})
+            console.log("user registered successfully")
+            return await createUserRepo.createUser(user)
+        }catch(error:any){
+            throw new Error(error.message)
+        }
+    }catch(error:any){
+        throw new Error(error.message)
+    }
 }
