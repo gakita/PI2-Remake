c5150b46fa7feab2928a137656ca5518addff3cb
gakita <raulgru5@gmail.com>
2025-10-31 01:20:31 -0300

    feat: rota de login ativada

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import HomePage from "../pages/HomePage"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LoginPage from "../pages/Auth/LoginPage"
 
 
 const RoutesApp: React.FC = () => {
@@ -8,7 +9,8 @@ const RoutesApp: React.FC = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                {/* <Route path="/login" element={<LoginPage />} /> */}
+                <Route path="/login" element={<LoginPage />} />
+                {/* <Route path="/register" element={<RegisterPage />} /> */}
             </Routes>
         </BrowserRouter>
     )
