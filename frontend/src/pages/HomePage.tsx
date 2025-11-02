b3f59d5d742ada0a726d2e111597b7475da0c234
gakita <raulgru5@gmail.com>
2025-11-02 02:09:18 -0300

    feat: adicionado o componente Footer para a página inicial

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import HeaderIndexPage from "../components/HeaderIndexPage"
 import SectionComponent from "../components/SectionComponent"
+import Footer from "../components/Footer"
 
 function HomePage(){
     return(
         <>
         <HeaderIndexPage />
         <SectionComponent />
+        <Footer />
         </>
     )
 }
