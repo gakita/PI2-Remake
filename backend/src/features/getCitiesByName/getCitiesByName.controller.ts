a9bb517d748faa862b87807ae095eefe0ae00535
gakita <raulgru5@gmail.com>
2025-11-02 13:14:27 -0300

    feat: adicionado tratamento de erro pelo zod

--- backend/src/features/getCitiesByName/getCitiesByName.controller.ts
+++ backend/src/features/getCitiesByName/getCitiesByName.controller.ts
@@ -2,6 +2,7 @@ import { validateCityNameSchema } from "./getCitiesByName.schema";
 import { getCitiesByNameService } from "./getCitiesByName.service";
 import { Request, Response, NextFunction } from "express";
 import { capitalizeFirstLetter } from "../../helper/capitalizeFirstLetter";
+import { z } from "zod";
 
 export async function getCitiesByNameController(req:Request, res:Response, next:NextFunction){
     try{
@@ -9,7 +10,10 @@ export async function getCitiesByNameController(req:Request, res:Response, next:
         const nameCapitalize = capitalizeFirstLetter(name)
         const cities = await getCitiesByNameService(nameCapitalize)
         return res.status(200).json(cities)
-    }catch(error){
+    }catch(error:any){
+        if (error instanceof z.ZodError){
+            return res.status(400).json({errors:error.message, status:400})
+        }
         next(error)
     }
 }
