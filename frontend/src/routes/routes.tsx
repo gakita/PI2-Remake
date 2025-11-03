7da174115f1b0b14fb1b21267ca57157c2eb89d1
gakita <raulgru5@gmail.com>
2025-11-03 01:21:43 -0300

    feat: ativada a criação de personagens

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import HomePage from "../pages/HomePage"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginPage from "../pages/Auth/LoginPage"
-
+import RegisterPage from "../pages/Auth/RegisterPage"
 
 const RoutesApp: React.FC = () => {
     return (
@@ -10,7 +10,7 @@ const RoutesApp: React.FC = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
-                {/* <Route path="/register" element={<RegisterPage />} /> */}
+                <Route path="/register" element={<RegisterPage />} />
             </Routes>
         </BrowserRouter>
     )
