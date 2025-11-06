8732f022d76c215ea0603c26e444b03b95283fd7
gakita <raulgru5@gmail.com>
2025-11-06 01:22:30 -0300

    refactor: reorganizada a estrutura da sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react"
 import api from "../Server"
 import "../styles/sidebar.css"
 import UserCard from "./userCard"
+import { imagePath } from "../hooks/imagePath"
 
 interface SidebarProps {
     isOpen: boolean;
@@ -25,53 +26,59 @@ function Sidebar({isOpen, toggle}: SidebarProps){
     return(
     <>
     
-    <nav className={["sidebar", open ? "active" : ""].join(" ")}>
-
-    {open? (
+    <nav className={["sidebar", open ? "close" : "open"].join(" ")}>
+        <div className="container-sidebar">
+            {open? (
         <>
-        <div className="nav-header">
-            <h1>Cryas Airways</h1>
-            <button onClick={() => setOpen(!open)}>
-                <i className={open ? "bi bi-x" : "bi bi-list"}></i>
-            </button>
+        <div className="sidebar-container">
+            <div className="sm-nav-header">
+            <i onClick={() => setOpen(!open)} className={open ? "bi bi-x" : "bi bi-list"}></i>
+            </div>
+            <div className="image-admin-nav-container img-overlay">
+                <img className="image-admin-nav" src={imagePath(user?.avatarPath!)} alt="" />
+            </div>
+            <ul className="nav-list">
+                <li>
+                    <a href="#"> <i className="bi bi-airplane"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i className="bi bi-people"></i></a>
+                </li>
+                <li>
+                <a href="#"><i className="bi bi-person"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i className="bi bi-calendar"></i></a>
+                </li>
+                <li><a onClick={handleLogout}><i className="bi bi-box-arrow-right"></i></a></li>
+            </ul>
         </div>
-        <ul className="nav-list">
-            <li><a href="/getFlights">Voos</a></li>
-            <li><a href="/getUsers">Usuarios</a></li>
-            <li><a href="/getClients">Clientes</a></li>
-            <li><a href="/getReservations">Reservas</a></li>
-            <li><a onClick={handleLogout}>Sair</a></li>
-        </ul>
         </>
     ):(
         <>
-        <div className="nav-header">
-            <h1>Cryas</h1>
-            <button onClick={() => setOpen(!open)}>
-                <i className={open ? "bi bi-x" : "bi bi-list"}></i>
-            </button>
+        <div className="sidebar-container">
+            <div className="nav-header">
+                <h1>Cryas</h1>
+                <i onClick={() => setOpen(!open)} className={["bi",open ? "bi-x" : "bi-list"].join(" ")}></i>
+            </div>
+            <h2>ADMIN</h2>
+            <div>
+                <div className="img-overlay">
+                    <UserCard homePage={false}/>
+                </div>
+            </div>
+            <ul className="nav-list">
+                <h2>MENU</h2>
+                <li><a href="#">Voos</a></li>
+                <li><a href="#">Usuarios</a></li>
+                <li><a href="#">Clientes</a></li>
+                <li><a href="#">Reservas</a></li>
+                <li><a onClick={handleLogout}>Sair</a></li>
+            </ul>
         </div>
-        <div>
-            <UserCard homePage={false}/>
-        </div>
-        <ul className="nav-list">
-            <p>MENU</p>
-            <li>
-                <a href="/getFlights"> <i className="bi bi-airplane"></i></a>
-            </li>
-            <li>
-                <a href="/getUsers"><i className="bi bi-people"></i></a>
-            </li>
-            <li>
-            <a href="/getClients"><i className="bi bi-person"></i></a>
-            </li>
-            <li>
-                <a href="/getReservations"><i className="bi bi-calendar"></i></a>
-            </li>
-            <li><a onClick={handleLogout}><i className="bi bi-box-arrow-right"></i></a></li>
-        </ul>
         </>
     )}
+        </div>
     </nav>
     </>
     )
